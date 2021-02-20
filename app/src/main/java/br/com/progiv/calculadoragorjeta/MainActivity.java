5be11ccf6a9dd593d7143ceac8d7e5adfa0f7b97
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-02-20 15:30:24 -0300

    Adicionado Watcher do campo valor da conta

--- app/src/main/java/br/com/progiv/calculadoragorjeta/MainActivity.java
+++ app/src/main/java/br/com/progiv/calculadoragorjeta/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.progiv.calculadoragorjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private double vrConta = 0.0; // valor da conta inserido pelo usuario
     private double percent = 0.015; // porcentagem inicial da gorjeta
     private TextView valorContaTextView; // mostra o valor da conta
-    private TextView ValorTotalTextView; // mostra o valor da conta calculada
+    private TextView valorTotalTextView; // mostra o valor da conta calculada
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,33 @@ public class MainActivity extends AppCompatActivity {
 
         //obter referencias para o TextView manipulados via programação
         valorContaTextView = (TextView)findViewById(R.id.valorConta);
+        valorTotalTextView = (TextView)findViewById(R.id.valorTotal);
+        valorTotalTextView.setText(currencyFormat.format(0));
 
+
+        //configura o receptor TextWatcher de valorContaEditText
+        EditText valorContaEditText = (EditText)findViewById(R.id.valorConta):
+        valorContaEditText.addTextChangedListener(valorContaEditWatcher);
     }
+    private final TextWatcher valorContaEditWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+           try{
+               vrConta = Double.parseDouble(s.toString());
+               valorTotalTextView.setText(currencyFormat.format(vrConta));
+           }catch (Exception ex){
+               String y = ex.getMessage();
+           }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
