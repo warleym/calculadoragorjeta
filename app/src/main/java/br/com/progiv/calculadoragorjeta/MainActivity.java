61cd083c73ee8cf31c927f932cd9bf6f8b16884e
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-02-20 14:48:48 -0300

    Configurando novos campos na tela e criando variaveis no código java.

--- app/src/main/java/br/com/progiv/calculadoragorjeta/MainActivity.java
+++ app/src/main/java/br/com/progiv/calculadoragorjeta/MainActivity.java
@@ -2,12 +2,28 @@ package br.com.progiv.calculadoragorjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    //objetos formatadores de moeda:
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+
+
+    private double vrConta = 0.0; // valor da conta inserido pelo usuario
+    private double percent = 0.015; // porcentagem inicial da gorjeta
+    private TextView valorContaTextView; // mostra o valor da conta
+    private TextView ValorTotalTextView; // mostra o valor da conta calculada
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //obter referencias para o TextView manipulados via programação
+        valorContaTextView = (TextView)findViewById(R.id.valorConta);
+
     }
 }
