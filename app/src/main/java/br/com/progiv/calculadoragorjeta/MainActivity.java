2c421c478ec8e54fb8b65be91763cbfb364ba4af
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-02-20 12:42:55 -0300

    Iniciando app calculadora gorjeta

---
+++ app/src/main/java/br/com/progiv/calculadoragorjeta/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.progiv.calculadoragorjeta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
